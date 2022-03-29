d04dd65e5b885b014b27b7c967c239842366ebf9
Ben Josua <51023701+benjosua@users.noreply.github.com>
2022-03-29 22:52:21 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+require('dotenv').config()
+const Discord = require('discord.js');
+const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES", "GUILD_VOICE_STATES", "GUILD_MEMBERS", "GUILD_PRESENCES"] });
+
+client.login(process.env.BOT_TOKEN)
+
+client.on("ready", () => {
+    console.log("Bot is ready")
+    })
+
+var timeouts  = new Map([])
+var delay = 5000
+var afkChannelID = "958066596843225178"
+
+client.on("voiceStateUpdate", (oldState, newState) =>
+{
+    if (newState.selfDeaf && newState.member.manageable && (newState.channelId != afkChannelID)) {
+        timeouts.set(newState.member.id, setTimeout(() => newState.member.voice.setChannel(afkChannelID), delay))
+}});
+
+client.on('voiceStateUpdate', (oldState, newState) => { 
+    if(newState.selfDeaf == false && (newState.channelId != afkChannelID)) {
+        console.log(timeouts.has(newState.member.id))
+        if (timeouts.has(newState.member.id)) {
+            clearTimeout(timeouts.get(newState.member.id));
+        }
+}});
